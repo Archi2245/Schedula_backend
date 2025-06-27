09eafd8dd09e7ea55fe2f4df5f5e530793406dfe
Archi <archigupta2245@gmail.com>
2025-06-27 13:37:46 +0530

    Fix export of typeOrmConfig

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,14 +1,17 @@
-import 'reflect-metadata';
-import { DataSource } from 'typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
-
 dotenv.config();
 
-export const AppDataSource = new DataSource({
+export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  url: process.env.DATABASE_URL, // ✅ Use DATABASE_URL in production
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/**/*.entity.{ts,js}'],
   synchronize: true,
+  autoLoadEntities: true,
   logging: true,
-  entities: ['src/**/*.entity.{ts,js}'],
-  migrations: ['src/migrations/*.ts'],
-});
+  logger: 'advanced-console',
+};
