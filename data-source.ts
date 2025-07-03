ae93f526f9424594b262f1e680aa8b04e3e9aa56
Archi <archigupta2245@gmail.com>
2025-07-03 10:08:26 +0530

    Add changes

--- data-source.ts
+++ data-source.ts
@@ -1,16 +1,28 @@
-import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities: ['src/**/*.entity.{ts,js}'],
-  migrations: ['src/migrations/*.ts'],
-  synchronize: false,
-});
+export const AppDataSource = new DataSource(
+  process.env.DATABASE_URL
+    ? {
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+        synchronize: false,
+        entities: ['dist/**/*.entity.js'],
+        migrations: ['dist/migrations/*.js'],
+      }
+    : {
+        type: 'postgres',
+        host: process.env.DB_HOST || 'localhost',
+        port: parseInt(process.env.DB_PORT || '5432'),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        synchronize: false,
+        entities: ['src/**/*.entity.{ts,js}'],
+        migrations: ['src/migrations/*.ts'],
+      }
+);
