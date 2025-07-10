4c153c4b13c6482dedd0550cbf6922e2f0b72738
Archi <archigupta2245@gmail.com>
2025-07-10 09:31:03 +0530

    handled errors

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -6,12 +6,14 @@ import { Appointment } from '../entities/appointment.entity';
 import { Doctor } from '../entities/doctor.entity';
 import { Patient } from '../entities/patient.entity';
 import { DoctorAvailability } from '../entities/doctor-availability.entity';
+import { TimeSlot } from '../entities/time-slot.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Appointment,
       Doctor,
+      TimeSlot,
       Patient,
       DoctorAvailability,
     ]),
